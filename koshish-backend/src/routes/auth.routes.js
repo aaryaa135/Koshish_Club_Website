ff9750f2bd252fab351a4c2aba62713869fffe31
dwag-cyber <tg1570@gmail.com>
2026-01-13 23:48:05 +0530

    br

--- koshish-backend/src/routes/auth.routes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-const { teacherRegister } = require("../controllers/auth.controller");
-
-router.post("/teacher-register", teacherRegister);
-
-module.exports = router;
